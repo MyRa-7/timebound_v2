ecdb59c6627697eb073bcde14c972ecbca8d4caa
Mythreyi <Ramesh@Rameshs-MacBook-Air.local>
2017-07-05 19:49:43 +0530

    Commit before changing ManyToMany to ForeignKey relation in db

--- mythbala/admin.py
+++ mythbala/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Week, Thing, Profile
 
+# class WeekInline(admin.StackedInline):
+# 	model = Week
+# 	extra = 1
+
+# class ProfileAdmin(admin.ModelAdmin):
+# 	fieldsets = [
+# 		(None,	{'fields': ['name']}),
+# 	]
+# 	inlines = [WeekInline]
+		
+admin.site.register(Profile)
 admin.site.register(Week)
 admin.site.register(Thing)
-admin.site.register(Profile)
